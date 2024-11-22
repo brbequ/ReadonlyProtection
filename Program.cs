c77812e0d0af8110ac8b8bd3a5b9c8ac44f3577e
Bruce Bequette <bbequette@concurrency.com>
2024-11-21 20:25:30 -0800

    Code cleanup

--- Program.cs
+++ Program.cs
@@ -1,13 +1,4 @@
-﻿// OneStream Code Assessment
-
-// Outline any issues/concerns with the implemented code
-// - The Console.WriteLine specification is using C++ syntax and needs to be changed to C#.
-// - Console.WriteLine() includes terminating the line with \n. Adding it in the format statement adds an extra \n
-// - Main isn't returning anything useful. The return type should be changed to void and delete the return statement.
-// - In class B, you cannot set a constant to a class. It must be set to a constant value.
-//   The solution is to change 'a' to readonly and initialize it in the constructor.
-
-B b = new B();
+﻿B b = new B();
 Console.WriteLine("{0} {1}", b.a.a, b.a.b);
 
 class A
